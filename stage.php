d1b84b24c54c7d4e85edd30dbd9311dff996e708
valentinjouffray <valentin.jouffray@gmail.com>
2022-03-07 23:45:49 +0100

    Amélioration de l'UI par ajout d'un head pour la liste, ajout dd'une colonne dans la BDD afin d'entrer un texte pour décrire le stage

--- stage.php
+++ stage.php
@@ -15,16 +15,33 @@ $statement->execute();
 $stages = $statement->fetchAll();
 
 ?>
+<h1>La liste des stages disponibles</h1>
+<div class="container" style="background-color: grey;">
+	<div class="row">
+		<div class="col-sm" style="outline-style: solid;">
+			<strong>Libellé</strong>
+		</div>
+		<div class="col-sm" style="outline-style: solid;">
+			<strong>Nom de l'entreprise</strong>
+		</div>
+		<div class="col-sm" style="outline-style: solid;">
+			<strong>Date de début</strong>
+		</div>
+		<div class="col-sm" style="outline-style: solid;">
+			<strong>Date de fin</strong>
+		</div>
+	</div>
+</div>
 <?php
 foreach ($stages as $stage){
 	?>
-	<a href="#"> <!-- Cliquer dessus affiche la fiche du stage -->
+	<a style="text-decoration: none; color: black;" href="#"> <!-- Cliquer dessus affiche la fiche du stage -->
 		<div class="container">
 			<div class="row">
-				<div class="col-sm"><?php echo $stage['stage_libelle'] ?></div>
-				<div class="col-sm"><?php echo $stage['stage_entreprise'] ?></div>
-				<div class="col-sm"><?php echo $stage['stage_date_debut'] ?></div>
-				<div class="col-sm"><?php echo $stage['stage_date_fin'] ?></div>
+				<div class="col-sm" style="outline-style: solid;"><?php echo $stage['stage_libelle'] ?></div>
+				<div class="col-sm" style="outline-style: solid;"><?php echo $stage['stage_entreprise'] ?></div>
+				<div class="col-sm" style="outline-style: solid;"><?php echo $stage['stage_date_debut'] ?></div>
+				<div class="col-sm" style="outline-style: solid;"><?php echo $stage['stage_date_fin'] ?></div>
 			</div>
 		</div>
 	</a>
